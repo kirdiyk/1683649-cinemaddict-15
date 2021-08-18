0b1dd1321ae2a225673dce6f42f4e20e628e87c2
kirdiyk <kirka.aka112@ya.ru>
2021-08-19 01:23:02 +0300

    правки

--- src/main.js
+++ src/main.js
@@ -9,39 +9,41 @@ import { createFilmListTemplate } from './view/list-film.js';
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 const siteFooterElement = document.querySelector('.footer');
-const siteFilmsContainer = siteMainElement.querySelector('.films');
-const siteFilmListContainer = siteMainElement.querySelector('.films-list__container');
-const siteFilmListElement = siteMainElement.querySelector('.films-list');
 
 const FILM_CARD = 5;
 const FILM_CARD_EXTRA = 2;
 
-const render = (place, template, container) => {
+const render = (template, container, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
 const renderFilmCards = (cardsCount, container) => {
   for (let i = 0; i < cardsCount; i++) {
-    render('beforeend', createCardFilmTemplate (), container);
+    render(createCardFilmTemplate (), container);
   }
 };
 
-render('beforeend', createSiteMenuTemplate(), siteMainElement);
-render('beforeend', profileTemplate(), siteHeaderElement);
-render('beforeend',  createFilmListTemplate(), siteMainElement);
+render(createSiteMenuTemplate(), siteMainElement);
+render(profileTemplate(), siteHeaderElement);
+render( createFilmListTemplate(), siteMainElement);
 
-render( 'beforeend', createFilmListExtraTemplate(), siteFilmsContainer);
-render( 'beforeend', createFilmListExtraTemplate(), siteFilmsContainer);
+const siteFilmsContainer = siteMainElement.querySelector('.films');
+
+render(createFilmListExtraTemplate(), siteFilmsContainer);
+render(createFilmListExtraTemplate(), siteFilmsContainer);
+
+const siteFilmListContainer = siteMainElement.querySelector('.films-list__container');
 
 renderFilmCards(FILM_CARD, siteFilmListContainer);
 
-render('beforeend', showMoreButtonTemplate(), siteFilmListElement);
+const siteFilmListElement = siteMainElement.querySelector('.films-list');
+render(showMoreButtonTemplate(), siteFilmListElement);
 
 const siteFilmListExtraContainer = [...siteFilmsContainer.querySelectorAll('.films-list--extra')];
-
 siteFilmListExtraContainer.forEach((item) => {
   const container = item.querySelector('.films-list__container');
   renderFilmCards(FILM_CARD_EXTRA, container);
 });
 
-render('afterend', createFilmDetailsTemplate(), siteFooterElement);
+
+render(createFilmDetailsTemplate(), siteFooterElement, 'afterend');
